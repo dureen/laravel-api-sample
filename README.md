3fe84188e49259e0bfc490b2bde73eb4b6241e74
dureen <dpdevproject@gmail.com>
2022-12-19 18:09:12 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Laravel 9.x REST API - Sample 
+
+## What is this?
+
+This is Laravel 9.x REST API project
+
+### Routes
+- `GET|HEAD`        api/v1/product ... product.index › Api\ProductController@index
+- `POST`            api/v1/product ... product.store › Api\ProductController@store
+- `GET|HEAD`        api/v1/product/{product} ... product.show › Api\ProductController@show
+- `PUT|PATCH`       api/v1/product/{product} ... product.update › Api\ProductController@update
+- `DELETE`          api/v1/product/{product} ... product.destroy › Api\ProductController@destroy
+
+
+
+## Installation, updates, and more setup
+
+Please read the original Laravel 9.x [README](README-Origin.md)
+
+## License
+[MIT](LICENSE) ?
+
+## Happy Coding!
---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use Illuminate\Support\Facades\Validator;
+
+class ProductController extends Controller
+{
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index()
+    {
+        // $products = Product::latest()->paginate(5);
+        $products = Product::all();
+        return new ProductResource(true, 'List Data Products', $products);
+    }
+
+    /**
+     * store
+     *
+     * @param mixed $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'     => 'required',
+            'price'   => 'required',
+        ]);
+
+        if ($validator->fails()) return response()->json($validator->errors(), 422);
+        $name = $request->name;
+        $price = $request->price;
+
+        $product = Product::create([
+            'name'     => $name,
+            'price'   => $price,
+        ]);
+
+        return new ProductResource(true, 'Product  Data was successfully added!', $product);
+    }
+
+    /**
+     * show
+     *
+     * @param  mixed $product
+     * @return void
+     */
+    public function show(Product $product)
+    {
+        return new ProductResource(true, 'Product data', $product);
+    }
+
+    /**
+     * update
+     *
+     * @param  mixed $request
+     * @param  mixed $product
+     * @return void
+     */
+    public function update(Request $request, Product $product)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'     => 'required',
+            'price'   => 'required',
+        ]);
+
+        if ($validator->fails()) return response()->json($validator->errors(), 422);
+
+        $product->update([
+            'name'     => $request->name,
+            'price'   => $request->price,
+        ]);
+
+        return new ProductResource(true, 'Product data was successfully updated!', $product);
+    }
+
+    /**
+     * destroy
+     *
+     * @param  mixed $product
+     * @return void
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return new ProductResource(true, 'Product data was successfully deleted!', null);
+    }
+
+}
+
